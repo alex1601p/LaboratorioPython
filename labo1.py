9df0c7462f064e4793d8a9699e8cde5f7cd54d49
Alex <ap16012001@gmail.com>
2021-10-19 17:41:31 -0600

    Script finalizado

--- labo1.py
+++ labo1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-c = input("Ingrese un caracter: ")
+c = input("Ingrese un caracter: ") #Mi carnet es C04873, por ende la piramide no sera inversa.
 n = int(input("Ingrese la base de la piramide: "))
 x = 1
 print (" ")
